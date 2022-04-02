efbcbb4f924e70bb3290e7f1fd5a68dfb6481f48
Paul Martin <hdsellpoint@gmail.com>
2022-04-02 15:14:58 +0200

    rdy

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(gamePlane)
 
 
 
-function makeWall([w, h, x, y, typ]){
+function makeWall([w, h, x, y, typ = "wall"]){
     const wall = document.createElement("div")
     wall.style.cssText =`
     position:absolute;
@@ -22,16 +22,24 @@ function makeWall([w, h, x, y, typ]){
     background-color: #00F7F7;
     left:${x}vw;
     top:${y}vh;
-    ` 
-wall.className = typ
+    display:flex;
+    justify-content:center;
+    align-items:center;`
+    
+    
+wall.className = typ;
+if(typ != "wall"){
+    wall.innerText = typ.toUpperCase();
+    
+}
 gamePlane.append(wall);
 }
 
 const mapa = [
     [20, 50, 0 ,25, 'start'],
-    [30, 20, 20, 40, 'wall'],
-    [30, 5, 50, 45, 'wall'],
-    [20, 40, 80, 25, 'meta']
+    [30, 10, 20, 45, 'wall'],
+    [30, 3, 50, 49, 'wall'],
+    [20, 30, 80, 30, 'meta']
 ]
 
 for(const wall of mapa){
@@ -65,11 +73,13 @@ const game = {
     },
     over(result){
         if(result) {
-            console.log("You Win")
+            // console.log("You Win")
+            guide.show("Wygrałeś!", "Możesz spróbować jeszcze raz!")
             
          }
          else{
-            console.log("You Lose")
+            // console.log("You Lose")
+            guide.show("Przegrałeś!", "Możesz spróbować jeszcze raz!")
          }
 
 
@@ -84,6 +94,64 @@ const game = {
 
 startButton.addEventListener("click", game.start);
 
+const guide = {
+    init(){
+        this.dom = document.createElement("div");
+        this.dom.className = "Guide";
+        this.wrapper = document.createElement("div");
+        this.wrapper.style.cssText = `
+        background-color: red;
+        padding:10vh 6vw;
+        width:65vw;
+        height:55vh;
+        text-align:center;
+        border-radius:10px;
+        box-shadow:0px 0px 20px black;
+        `
+        this.tittle = document.createElement("h1");
+        this.tittle.innerText = "HI";
+        this.message = document.createElement("p");
+        this.message.innerText = "This is a guide";
+
+        this.button = document.createElement("button");
+        this.button.innerText = "OK";
+
+        // this.button.addEventListener("click", this.close)
+        this.button.addEventListener("click", () => {
+            this.close()
+        })
+
+        
+        
+        this.wrapper.append(this.tittle);        
+        this.wrapper.append(this.message);
+        this.wrapper.append(this.button);
+        this.dom.append(this.wrapper);
+        document.body.append(this.dom);
+    },
+    
+
+    close(){
+        
+        this.dom.style.display = "none";
+
+        
+    },
+
+    show(tittle, message){
+        console.log("show")
+        this.tittle.innerText = tittle;
+        this.message.innerText = message;
+        this.dom.style.display = "flex";
+
+    }
+
+}
+guide.init();
+guide.show(
+    "Witaj w grze!",
+    "Kliknij start i przedostań się kursorem do mety"
+)
 
 
 
